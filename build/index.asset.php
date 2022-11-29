e2dc336c471688a8be7ff98e32b0920164da2615
Muntasir Mahmud Aumio <secretspeed.aumz@gmail.com>
2022-11-29 20:33:16 +0600

    Release version

---
+++ build/index.asset.php
@@ -0,0 +1,1 @@
+<?php return array('dependencies' => array('lodash', 'react', 'react-dom', 'wp-block-editor', 'wp-components', 'wp-compose', 'wp-data', 'wp-element', 'wp-hooks', 'wp-i18n', 'wp-primitives'), 'version' => 'b7be384597af534a61c5');
